314db6d9d16bd99a724479a0b7b9b2ba9c31c4f1
Jean <105326911+JeanzitoDev@users.noreply.github.com>
2024-09-06 22:08:51 -0300

    Add files via upload
    herança (precisa corrigir a anotação de Override da classe Assistente)

---
+++ Lista4/src/br/edu/fatecfranca/Main.java
@@ -0,0 +1,11 @@
+package br.edu.fatecfranca;
+
+import br.edu.fatecfranca.Assistente;
+
+public class Main {
+    public static void main(String[] args) {
+        Assistente ass1 = new Assistente("Jean", "Rua do cu de judas", "123.456.789-11", 3000, 4);
+
+        System.out.println(ass1);
+    }
+}
